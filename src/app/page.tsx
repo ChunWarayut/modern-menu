a02318d67b0e4f72c174a97df7a3d19f2a6b7ff0
ChunWarayut <warayut2555@gmail.com>
2023-06-22 02:06:14 +0700

    splash screen page button blur-xl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function SplashScreenPage () {
         >
           <span className='absolute top-0 left-0 w-full h-full rounded-3xl opacity-50 filter blur-sm bg-gradient-to-br from-amber-600 to-yellow-500'></span>
           <span className='h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded-3xl opacity-50 from-amber-600 to-yellow-500'></span>
-          <span className='absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded-3xl shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-amber-600 to-yellow-500'></span>
+          <span className='absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded-3xl shadow-xl bg-gradient-to-br filter group-active:opacity-0 blur-xl group-focus::blur-xl group-hover:blur-xl from-amber-600 to-yellow-500'></span>
           <span className='absolute inset-0 w-full h-full transition duration-200 ease-out rounded-3xl bg-gradient-to-br to-amber-600 from-yellow-500'></span>
           <span className='relative'>Continue</span>
         </a>
